ffb6639432625104f06333aea1c34b1211ea1f06
DIMI7R0V <danito1718@gmail.com>
2025-05-31 01:02:36 +0300

    1

--- PCStore.Models/Configurations/MongoDBConfiguration.cs
+++ PCStore.Models/Configurations/MongoDBConfiguration.cs
@@ -4,10 +4,6 @@
     {
         public string ConnectionString { get; set; } = string.Empty;
 
-<<<<<<< HEAD
         public string DatabaseName { get; set; } = string.Empty; 
-=======
-        public string DatabaseName { get; set; } = string.Empty;
->>>>>>> 7f7db6a57b4d20d5df953426f620152ebef76aa6
     }
 }
